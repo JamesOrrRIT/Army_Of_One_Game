9d3ac851fb511ffb17cf540a5f3644f60951d3d3
jmb8220 <47392893+jmb8220@users.noreply.github.com>
2020-06-27 16:07:48 -0400

    Add files via upload
    Uploaded Player.cs and Tile.cs

---
+++ ArmyOfOne/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ArmyOfOne
+{
+    class Player : GameObject
+    {
+        //Attributes
+        int health;
+        int score;
+        private KeyboardState kState;
+
+        private int prevX;
+        private int prevY;
+
+        public Player(int x, int y, int width, int height) : base(x, y, width, height)
+        {
+            hitbox = new Rectangle(x, y, width, height);
+        }
+
+        //Updating the player
+        public void update()
+        {
+            //Saving the previous coordinants
+            prevX = hitbox.X;
+            prevY = hitbox.Y;
+
+            //Moving the image with the arrow keys
+            kState = Keyboard.GetState();
+            if (kState.IsKeyDown(Keys.Left))
+            {
+                hitbox.X -= 5;
+                return;
+            }
+            if (kState.IsKeyDown(Keys.Right))
+            {
+                hitbox.X += 5;
+                return;
+            }
+            if (kState.IsKeyDown(Keys.Up))
+            {
+                hitbox.Y -= 5;
+                return;
+            }
+            if (kState.IsKeyDown(Keys.Down))
+            {
+                hitbox.Y += 5;
+                return;
+            }
+
+        }
+
+        public void wallCollision(GameObject g)
+        {
+            if(g.hitbox.Intersects(hitbox))
+            {
+                hitbox.X = prevX;
+                hitbox.Y = prevY;
+            }
+        }
+
+    }
+}
---
+++ ArmyOfOne/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ArmyOfOne
+{
+    class Tile : GameObject
+    {
+        public Tile(int x, int y, int width, int height) : base(x, y, width, height)
+        {
+            hitbox = new Rectangle(x, y, width, height);
+        }
+    }
+}
