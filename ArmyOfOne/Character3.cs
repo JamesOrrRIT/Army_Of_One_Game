cf1ae5becfe48070447ee4d6028a2e5bd9472a5d
rhkoenig <rhkoenig@outlook.com>
2020-06-27 21:56:14 -0400

    added 2 new characters, as duplicates of the first to be edited later

---
+++ ArmyOfOne/Character3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyOfOne
+{
+    class Character3 : Player
+    {
+        //character type 3
+        public Character3(int x, int y, int width, int height) : base(x, y, width, height, 3, 5)
+        {
+
+        }
+    }
+}
---
+++ ArmyOfOne/Character4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyOfOne
+{
+    class Character4 : Player
+    {
+        //character type 4
+        public Character4(int x, int y, int width, int height) : base(x, y, width, height, 3, 5)
+        {
+
+        }
+    }
+}
